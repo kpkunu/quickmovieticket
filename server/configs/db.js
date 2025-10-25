d3d742783dab3110c83fc7fadae5625107451c3f
kamalakanta parida <rohandas543271@gmail.com>
2025-10-25 20:56:36 +0530

    change in db.js removed /quickshow

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,13 +1,12 @@
 import mongoose from 'mongoose';
 
-const connectDB = async () =>{
-    try {
-        mongoose.connection.on('connected', ()=> console.log('Database connected'));
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`)
-    } catch (error) {
-        console.log(error.message);
-        
-    }
-}
+const connectDB = async () => {
+  try {
+    mongoose.connection.on('connected', () => console.log('Database connected'));
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.log('MongoDB Connection Error:', error.message);
+  }
+};
 
 export default connectDB;
